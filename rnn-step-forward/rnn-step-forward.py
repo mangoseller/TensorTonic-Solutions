235e4c7b71fdca0c795e5e96e1ea8fb07357b425
Sam <101512334+mangoseller@users.noreply.github.com>
2025-12-21 08:26:52 +1100

    Add solution for rnn-step-forward

---
+++ rnn-step-forward/rnn-step-forward.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+def rnn_step_forward(x_t, h_prev, Wx, Wh, b):
+    
+    return np.tanh(
+        np.array(x_t) @ np.array(Wx) + \
+        np.array(h_prev) @ np.array(Wh) + b
+    )
+
