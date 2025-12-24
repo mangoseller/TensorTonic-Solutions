a444b4298b400709afacebfab48c439257d30920
Sam <101512334+mangoseller@users.noreply.github.com>
2025-12-24 11:18:42 +1100

    Update solution for nesterov-momentum

--- nesterov-momentum/nesterov-momentum.py
+++ nesterov-momentum/nesterov-momentum.py
@@ -1,10 +1,8 @@
 import numpy as np
 
 def nesterov_momentum_step(w, v, grad, lr=0.01, momentum=0.9):
-    w = np.array(w)
-    v = np.array(v)
-    grad = np.array(grad)
-    w_look = w - momentum*v
-    v = momentum*v + lr*grad
-    w -= v
-    return (w, v)
+    w, v, grad = np.array(w), np.array(v), np.array(grad)
+    return (
+        w - (momentum * v + lr*grad),
+        momentum*v + lr*grad
+    )
