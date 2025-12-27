a4bc626d67be79236e84ac2cc4f7e94c8e3be9e6
Sam <101512334+mangoseller@users.noreply.github.com>
2025-12-28 08:51:03 +1100

    Update solution for adam-optimizer

--- adam-optimizer/adam-optimizer.py
+++ adam-optimizer/adam-optimizer.py
@@ -1,9 +1,12 @@
 import numpy as np
 
 def adam_step(param, grad, m, v, t, lr=1e-3, beta1=0.9, beta2=0.999, eps=1e-8):
-    m_t = (beta1 * m) + (1 - beta1) * grad
-    v_t = (beta2 * v) + (1 - beta2) * grad**2
-    m_hat = m_t / (1 - (beta1**t))
-    v_hat = v_t / (1 - (beta2**t))
-    param_n = param - (lr * m_hat) / (np.sqrt(v_hat) + eps)
-    return param_n, m_t, v_t
+    return (lambda m_t, v_t: (
+        param - lr * (m_t / (1-beta1**t)) / (np.sqrt(v_t / (1-beta2**t)) + eps),
+        m_t,
+        v_t
+    ))(
+        beta1 * m + (1-beta1) * grad,
+        beta2 * v + (1-beta2) * grad ** 2
+    )
+    
