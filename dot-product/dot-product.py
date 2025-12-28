87656ea2bfe90b60010ff0173079697c57c51f2d
Sam <101512334+mangoseller@users.noreply.github.com>
2025-12-28 16:48:57 +1100

    Update solution for dot-product

--- dot-product/dot-product.py
+++ dot-product/dot-product.py
@@ -2,5 +2,5 @@ import numpy as np
 
 def dot_product(x, y):
     def _raise(e): raise e
-    return _raise(ValueError("Mismatched lengths!")) if np.asarray(x).shape != np.asarray(y).shape \
-    else np.sum(np.asarray(x)*np.asarray(y), dtype=float)
+    return _raise(ValueError("Mismatched lengths!")) if (x_arr := np.asarray(x)).shape != (y_arr := np.asarray(y)).shape \
+    else np.sum(x_arr * y_arr, dtype=float)
