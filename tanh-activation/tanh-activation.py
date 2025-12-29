a7ab01b20f83fcb84d2722a9be6b18b0aa20369e
Sam <101512334+mangoseller@users.noreply.github.com>
2025-12-30 09:59:30 +1100

    Update solution for tanh-activation

--- tanh-activation/tanh-activation.py
+++ tanh-activation/tanh-activation.py
@@ -3,6 +3,6 @@ import numpy as np
 def tanh(x):
     return np.atleast_1d(
         (lambda n: (np.exp(n) - np.exp(-n)) / (np.exp(n) + np.exp(-n)))
-        (np.clip(np.asarray(x, dtype=np.float64), -600, 600))
+        (np.clip(np.asarray(x, dtype=np.float64), -600, 600)) # Handle numerical overflow/underflow
         )
         
