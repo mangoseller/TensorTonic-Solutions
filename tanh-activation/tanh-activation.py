3fb2058b121c9d48c5a1e0682806bce3a29ddcfd
Sam <101512334+mangoseller@users.noreply.github.com>
2025-12-30 09:58:40 +1100

    Add solution for tanh-activation

---
+++ tanh-activation/tanh-activation.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+def tanh(x):
+    return np.atleast_1d(
+        (lambda n: (np.exp(n) - np.exp(-n)) / (np.exp(n) + np.exp(-n)))
+        (np.clip(np.asarray(x, dtype=np.float64), -600, 600))
+        )
+        
