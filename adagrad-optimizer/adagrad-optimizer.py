6fbd15712c83f7f2c37e1ebdac15ddbed2d0a7c7
Sam <101512334+mangoseller@users.noreply.github.com>
2025-12-31 08:51:05 +1100

    Update solution for adagrad-optimizer

--- adagrad-optimizer/adagrad-optimizer.py
+++ adagrad-optimizer/adagrad-optimizer.py
@@ -1,14 +1,10 @@
 import numpy as np
 
 def adagrad_step(w, g, G, lr=0.01, eps=1e-8):
-    w = np.asarray(w)
-    g = np.asarray(g)
-    G = np.asarray(G)
-    g_t = G + g**2
-    w_t = w - (lr * g) / (np.sqrt(g_t) + eps)  
-    return (w_t, g_t)
-    """
-    Perform one AdaGrad update step.
-    """
-    # Write code here
-    pass
+    return (lambda new_g, old_w: (
+       old_w - (lr * np.asarray(g)) / (np.sqrt(new_g) + eps), 
+       new_g  
+    ))(
+        np.asarray(G) + np.asarray(g)**2,
+        np.asarray(w)
+    )
