ecc956ae468c0b51e2d0c11716b5f06c01fa8ca8
Sam <101512334+mangoseller@users.noreply.github.com>
2026-01-01 10:54:31 +1100

    Update solution for angle-between-3d

--- angle-between-3d/angle-between-3d.py
+++ angle-between-3d/angle-between-3d.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 def angle_between_3d(v, w):
-    return np.nan if (np.linalg.norm((v_arr := np.asarray(v))) == 0 or \
-    np.linalg.norm((w_arr := np.asarray(w))) == 0) else \
-    np.arccos(np.clip((np.dot(v_arr, w_arr)) / (np.linalg.norm(v_arr) * np.linalg.norm(w_arr)))) 
+    return np.nan if ((v_norm := np.linalg.norm((v_arr := np.asarray(v)))) == 0 or \
+    (w_norm := np.linalg.norm((w_arr := np.asarray(w)))) == 0) else \
+    np.arccos((np.dot(v_arr, w_arr)) / (w_norm * v_norm))
+
