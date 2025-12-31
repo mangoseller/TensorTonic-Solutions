20860d00f26694ec89ce407e6770dd28749ae828
Sam <101512334+mangoseller@users.noreply.github.com>
2026-01-01 10:24:07 +1100

    Update solution for rmsprop-optimizer

--- rmsprop-optimizer/rmsprop-optimizer.py
+++ rmsprop-optimizer/rmsprop-optimizer.py
@@ -1,9 +1,12 @@
 import numpy as np
 
 def rmsprop_step(w, g, s, lr=0.001, beta=0.9, eps=1e-8):
-    w = np.asarray(w)
-    g = np.asarray(g)
-    s = np.asarray(s)
-    s_t = (beta * s) + ((1-beta) * g**2)
-    w_t = w - (lr / (np.sqrt(s_t) + eps)) * g
-    return w_t, s_t
+    return (lambda w_arr, g_arr, s_t: (
+        w - (lr / (np.sqrt(s_t) + eps)) * g,
+        s_t
+    ))(
+     np.asarray(w),
+     np.asarray(g),
+     (beta * np.asarray(s)) + ((1-beta) * np.asarray(g)**2)
+     )
+
