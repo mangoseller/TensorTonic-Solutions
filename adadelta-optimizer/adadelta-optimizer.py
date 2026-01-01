499495b4435c9cf006310769b33c0f7d8c947ae3
Sam <101512334+mangoseller@users.noreply.github.com>
2026-01-02 10:08:05 +1100

    Update solution for adadelta-optimizer

--- adadelta-optimizer/adadelta-optimizer.py
+++ adadelta-optimizer/adadelta-optimizer.py
@@ -1,10 +1,16 @@
 import numpy as np
 
 def adadelta_step(w, grad, E_grad_sq, E_update_sq, rho=0.9, eps=1e-6):
-  w, grad, E_grad_sq, E_update_sq = (np.asarray(x) for x in [w, grad, E_grad_sq, E_update_sq])
-  e_g = (rho * E_grad_sq) + (1-rho) * (grad ** 2)
-  d_t = (-np.sqrt((E_update_sq + eps) / (e_g + eps))) * grad
-  e_dt = (rho * E_update_sq) + (1 - rho) * (d_t **2)
-  w_t = w + d_t
-  return (w_t, e_g, e_dt)
+  return (lambda w_t, grad_, E_grad_sq_a, E_update_sq_a: (
+    w_t + (d_t := (-np.sqrt(
+      (E_update_sq_a + eps) / ((e_t := (rho * E_grad_sq_a) + (1-rho) * (grad_ ** 2)) + eps))) * grad_),
+      e_t,
+      (rho * E_update_sq_a) + (1 - rho) * (d_t **2)
+  ))(
+    np.asarray(w),
+    np.asarray(grad),
+    np.asarray(E_grad_sq),
+    np.asarray(E_update_sq)
+  )
+
     
