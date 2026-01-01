935c809aca8f5242438cf85805adba21815edcfd
Sam <101512334+mangoseller@users.noreply.github.com>
2026-01-02 09:50:46 +1100

    Update solution for nadam-optimizer

--- nadam-optimizer/nadam-optimizer.py
+++ nadam-optimizer/nadam-optimizer.py
@@ -1,11 +1,13 @@
 import numpy as np
 
 def nadam_step(w, m, v, grad, lr=0.002, beta1=0.9, beta2=0.999, eps=1e-8):
-    w = np.asarray(w)
-    m = np.asarray(m)
-    v = np.asarray(v)
-    grad = np.asarray(grad)
-    m_t = (beta1*m) + (1-beta1) * grad
-    v_t = (beta2*v) + (1-beta2) * grad**2
-    w_t = w - (lr * ((beta1*m_t) + ((1-beta1) * grad)) / (np.sqrt(v_t) + eps))
-    return (w_t, m_t, v_t)
+    return (lambda w_t, m_t, v_t, grad_:(
+        w - (lr * ((beta1*m_t) + ((1-beta1) * grad_)) / (np.sqrt(v_t) + eps)),
+        m_t,
+        v_t
+    ))(
+        np.asarray(w),
+        (beta1*np.asarray(m)) + (1-beta1) * (grad_t := np.asarray(grad)),
+        (beta2*np.asarray(v)) + (1-beta2) * grad_t**2,
+        grad_t      
+    )
