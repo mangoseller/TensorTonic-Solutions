d2868509e24e793797ae96f625df36fcea39153c
Sam <101512334+mangoseller@users.noreply.github.com>
2026-01-03 10:23:21 +1100

    Update solution for focal-loss

--- focal-loss/focal-loss.py
+++ focal-loss/focal-loss.py
@@ -1,6 +1,8 @@
 import numpy as np
 
 def focal_loss(p, y, gamma=2.0):
-    return np.vectorize(lambda p_l, y_l:( -(1-p_l)**gamma * (y_l * np.log(np.clip(p_l, 1e-15, (1-1e-15)))) - (p_l**gamma * (1-y_l) * np.log(np.clip((1-p_l), 1e-15, (1-1e-15))))))(p, y).mean()
+    return np.vectorize(
+        lambda p_l, y_l:(-(1-p_l)**gamma * (y_l * np.log(np.clip(p_l, 1e-15, (1-1e-15)))) - (p_l**gamma * (1-y_l) * np.log(np.clip((1-p_l), 1e-15, (1-1e-15)))))
+        )(p, y).mean()
 
 
