4df162788265cf8d5471af8d4efd9ecef8bec175
Sam <101512334+mangoseller@users.noreply.github.com>
2026-01-04 11:19:56 +1100

    Update solution for poisson-pmf-cdf

--- poisson-pmf-cdf/poisson-pmf-cdf.py
+++ poisson-pmf-cdf/poisson-pmf-cdf.py
@@ -1,8 +1,7 @@
 import numpy as np
 
 def poisson_pmf_cdf(lam, k):
-    pm = 0
-    for i in range(k+1):
-        pm += (np.exp(-lam) * lam ** i) / np.prod(np.arange(1, i+1))
+    return (lambda pmf: ((pmf(k), sum(pmf(i) for i in range(k+1)))))(
+        lambda i: (np.exp(-lam) * lam ** i) / np.prod(np.arange(1, i+1))
+        )
 
-    return((np.exp(-lam) * lam ** k) / np.prod(np.arange(1, k+1)), pm)
