60dd5a656651ea5e48c4d2977bd72f5b8afdb31a
Sam <101512334+mangoseller@users.noreply.github.com>
2026-01-05 10:06:05 +1100

    Update solution for silhouette-score

--- silhouette-score/silhouette-score.py
+++ silhouette-score/silhouette-score.py
@@ -1,20 +1,18 @@
 import numpy as np
 
 def silhouette_score(X, labels):
-    
-    dist = np.linalg.norm(X[:, None, :] - X, axis=2)
-    unique_labels = np.unique(labels)
-    onehot = (labels[:, None] == unique_labels).astype(float)
-    sizes = onehot.sum(axis=0)
+    return (lambda dist, onehot: (lambda sizes, idx, sum_to:
+    np.where((denom := np.maximum(
+        (a := sum_to[np.arange(len(X)), idx] / np.maximum(sizes[idx] - 1, 1)),
+        (b := np.where(np.arange(len(sizes)) == idx[:, None], np.inf, sum_to / sizes).min(axis=1))
+    )) == 0, 0, (b - a) / denom).mean()
+    )(
+        onehot.sum(axis=0),
+        onehot.argmax(axis=1),
+        dist @ onehot
+    ))(
+        np.linalg.norm(X[:, None] - X, axis=2),
+        (labels[:, None] == np.unique(labels)).astype(float)
+    )
 
-    sum_to_clusters = dist @ onehot 
-    cluster_idx = onehot.argmax(axis=1)
-
-    a = sum_to_clusters[np.arange(len(X)), cluster_idx] / np.maximum(sizes[cluster_idx] - 1, 1)
-    mean_to_clusters = sum_to_clusters / sizes
-    mean_to_clusters[np.arange(len(X)), cluster_idx] = np.inf
-    b = mean_to_clusters.min(axis=1)
-
-    return np.where(np.maximum(a, b) == 0, 0, 
-    (b - a) / np.maximum(a, b)).mean()
     
