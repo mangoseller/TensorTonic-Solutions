00b6a311ed0d8bde9344f62c584a3ee74c3717be
Sam <101512334+mangoseller@users.noreply.github.com>
2026-01-06 12:03:41 +1100

    Update solution for triplet-loss

--- triplet-loss/triplet-loss.py
+++ triplet-loss/triplet-loss.py
@@ -3,7 +3,7 @@ import numpy as np
 def triplet_loss(anchor, positive, negative, margin=1.0):
     return (lambda d, an, p, n: (max(0, (d(an, p) - d(an, n) + margin))))(
         lambda x, y: np.linalg.norm(x - y)**2,
-        (an := np.asarray(anchor)),
-        (p := np.asarray(positive)),
-        (n := np.asarray(negative)),
+        np.asarray(anchor),
+        np.asarray(positive),
+        np.asarray(negative),
     ) 
