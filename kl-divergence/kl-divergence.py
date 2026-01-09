9ed585d9600baedbed4f5fdb030d7662bb4bf497
Sam <101512334+mangoseller@users.noreply.github.com>
2026-01-10 09:15:00 +1100

    Update solution for kl-divergence

--- kl-divergence/kl-divergence.py
+++ kl-divergence/kl-divergence.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 def kl_divergence(p, q, eps=1e-12):
-    return np.vectorize((lambda p_a, q_a: (
+    return np.vectorize((lambda p_a, q_a:(
         np.where(p_a > 0, p_a * np.log(p_a / q_a), 0))))(
         np.asarray(p) + eps,
         np.asarray(q) + eps
