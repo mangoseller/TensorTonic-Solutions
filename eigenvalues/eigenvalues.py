2aceba02c8a5213cfe4307ff07c08c4e67b345b4
Sam <101512334+mangoseller@users.noreply.github.com>
2026-01-12 09:36:32 +1100

    Update solution for eigenvalues

--- eigenvalues/eigenvalues.py
+++ eigenvalues/eigenvalues.py
@@ -1,9 +1,5 @@
 import numpy as np
 
 def calculate_eigenvalues(matrix):
-    if not matrix or (matrix := np.asarray(matrix)).ndim != 2 or matrix.shape[0] != matrix.shape[1]:
-        return None
-    real = -(eigenvalues := np.linalg.eigvals(matrix)).real
-    im = -eigenvalues.imag
-    idx = np.lexsort((im, real))
-    return eigenvalues[idx]
+    return None if (not matrix or (matrix := np.asarray(matrix)).ndim != 2 or matrix.shape[0] != matrix.shape[1]) else (eigvals := np.linalg.eigvals(matrix))[np.lexsort((-eigvals.imag, -eigvals.real))]
+
