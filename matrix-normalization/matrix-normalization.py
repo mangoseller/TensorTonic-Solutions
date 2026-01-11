d66e01e961dca2cfcc3f14780356be467fe07188
Sam <101512334+mangoseller@users.noreply.github.com>
2026-01-12 09:44:34 +1100

    Update solution for matrix-normalization

--- matrix-normalization/matrix-normalization.py
+++ matrix-normalization/matrix-normalization.py
@@ -1,18 +1,10 @@
 import numpy as np
 
 def matrix_normalization(matrix, axis=None, norm_type='l2'):
-    matrix = np.asarray(matrix)
-    if matrix.ndim != 2:
-        return None
-        
-    match norm_type:
-        case 'max':
-            return matrix / np.where((
-                norms := np.linalg.norm(matrix, ord=np.inf, axis=axis, keepdims=True)) == 0, 1, norms)
-        case 'l1':
-            return matrix / np.where((
-                norms := np.linalg.norm(matrix, ord=1, axis=axis, keepdims=True)) == 0, 1, norms)
-        case 'l2':
-            return matrix / np.where(
-                (norms := np.sqrt(np.sum(matrix ** 2, axis=axis, keepdims=True))) == 0, 1, norms)
+    return None if (matrix := np.asarray(matrix)).ndim != 2 else \
+    {
+        "max": lambda: matrix / np.where((norms := np.linalg.norm(matrix, ord=np.inf, axis=axis, keepdims = True)) == 0, 1, norms),
+        "l1": lambda: matrix / np.where((norms := np.linalg.norm(matrix, ord=1, axis=axis, keepdims = True)) == 0, 1, norms),
+        "l2": lambda: matrix / np.where((norms := np.sqrt(np.sum(matrix**2, axis=axis, keepdims=True))) == 0, 1, norms)
+    }[norm_type]()
 
