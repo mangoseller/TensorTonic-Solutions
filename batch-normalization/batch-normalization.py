cf50f4cd81c67070cc382d401bf8afc927fff146
Sam <101512334+mangoseller@users.noreply.github.com>
2026-01-20 07:31:41 +1100

    Update solution for batch-normalization

--- batch-normalization/batch-normalization.py
+++ batch-normalization/batch-normalization.py
@@ -1,22 +1,12 @@
 import numpy as np
 
 def batch_norm_forward(x, gamma, beta, eps=1e-5):
-
-    fourD = len(x.shape) == 4
-    sample = x.copy()
-    if fourD:
-        gamma = gamma[None, :, None, None]
-        beta = beta[None, :, None, None]
-        mu = np.mean(sample, axis=(0, 2, 3), keepdims=True)
-        var = np.var(sample, axis=(0, 2, 3), keepdims=True)
-    else:
-        mu = np.mean(sample, axis=0, keepdims=True)
-        var = np.var(sample, axis=0, keepdims=True) 
-
-    x_hat = (x - mu) / np.sqrt(var + eps)
-    y_hat = (gamma * x_hat) + beta
-
-    return y_hat    
-
+    return (lambda x, gam, beta, mu, var: (gam * (x - mu) / np.sqrt(var + eps)) + beta)(
+        x,
+        gamma[None, :, None, None] if (fourD := len(x.shape) == 4) else gamma,
+        beta[None, :, None, None] if fourD else beta,
+        np.mean(x.copy(), axis=(0, 2, 3) if fourD else 0, keepdims=True),
+        np.var(x.copy(), axis=(0, 2, 3) if fourD else 0, keepdims=True)
+    )
 
     
